58e51300b42c2b15fed1387e040c1fd03e428733
Kareene Butler <Kareenebutler5@gmail.com>
2024-08-05 21:24:02 -0400

    projects p&g r good

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,15 @@
-// build your `Project` model here
+const db = require('../../data/dbConfig')
+
+const getAll = () => {
+    return db('projects')
+}
+
+async function add(project) {
+    const [id] = await db('projects').insert(project);
+    return db('projects').where({ project_id: id }).first();
+  }
+
+module.exports = {
+    getAll,
+    add,
+}
--- api/project/router.js
+++ api/project/router.js
@@ -3,4 +3,40 @@ const Projects = require('./model')
 
 const router = express.Router()
 
+router.get('/', async (req, res, next) => {
+    try{
+        const projects = await Projects.getAll()
+
+        const transformedProjects = projects.map(project => ({
+            ...project,
+            project_completed: Boolean(project.project_completed)
+          }))
+        res.json(transformedProjects)
+    } catch (err) {
+        next (err)
+    }
+})
+
+router.post('/', async (req, res, next) => {
+    try{
+        const { project_name, project_description, project_completed } = req.body
+
+        if (!project_name) {
+            return res.status(400).json({ message: 'Project name is required' });
+          }
+          const newProject = {
+            project_name,
+            project_description,
+            project_completed: project_completed !== undefined ? project_completed : false,
+          }
+          const createdProject = await Projects.add(newProject)
+
+          createdProject.project_completed = Boolean(createdProject.project_completed)
+
+          res.status(201).json(createdProject)
+    } catch (err) {
+        next(err)
+    }
+})
+
 module.exports = router 
