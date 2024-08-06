59ecb2a2fcf5d9fbab65a42db2a1e0c6676dc22a
Kareene Butler <Kareenebutler5@gmail.com>
2024-08-06 12:13:42 -0400

    get and post resources g

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,21 @@
-// build your `Resource` model here
+const db = require('../../data/dbConfig')
+
+const getAll = () => {
+    return db('resources')
+}
+
+async function add(resource) {
+    const [id] = await db('resources').insert(resource)
+    return db('resources').where({ resource_id: id }).first()
+}
+
+const findByName = async (name) => {
+    return await db('resources').where({ resource_name: name }).first()
+}
+
+
+module.exports = {
+    getAll,
+    add,
+    findByName,
+}
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,40 @@
-// build your `/api/resources` router here
+const express = require('express')
+const Resource = require('./model')
+
+const router = express.Router()
+
+router.get('/', async (req, res, next) => {
+    try{
+        const resources = await Resource.getAll()
+        if (!resources.length) {
+            return res.status(404).json({ message: 'No resources found' })
+        }
+        res.json(resources)
+    } catch (err) {
+        next (err) 
+    }
+})
+
+router.post('/', async (req, res, next) => {
+    try{
+        const { resource_name, resource_description } = req.body
+
+        if (!resource_name) {
+            return res.status(400).json({ message: 'Resource name is required' })
+        }
+
+        const existingResource = await Resource.findByName(resource_name)
+        if (existingResource) {
+            return res.status(409).json({ message: 'Resource name already exists' })
+        }
+
+        const newResource = { resource_name, resource_description }
+        const createdResource = await Resource.add(newResource)
+
+        res.status(201).json(createdResource)
+    } catch (err) {
+
+    }
+})
+
+module.exports = router
