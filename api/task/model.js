9e42fd5cf343cb2398a7a77dbc745600ee094a93
Kareene Butler <Kareenebutler5@gmail.com>
2024-08-06 14:25:06 -0400

    close

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,15 @@
-// build your `Task` model here
+const db = require('../../data/dbConfig');
+
+const getAll = () => {
+  return db('tasks')
+}
+
+async function add(task) {
+    const [task_id] = await db('tasks').insert(task)
+    return db('tasks').where({ task_id: id }).first
+}
+
+module.exports = {
+  getAll,
+  add,
+}
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,34 @@
-// build your `/api/tasks` router here
+const express = require('express');
+const db = require('../../data/dbConfig')
+const Task = require('./model');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+      const tasks = await Task.getAll()
+
+      res.json(tasks)
+    } catch (err) {
+      res.status(500).json({ message: 'Error fetching tasks' });
+    }
+  })
+
+  router.post('/', async (req, res) => {
+    try {
+        const { task_notes, task_description, task_completed, project_id } = req.body
+
+        const newTask = {
+            task_notes,
+            task_description,
+            task_completed: task_completed !== undefined ? task_completed : false,
+            project_id,
+        }
+        const createdTask = await Task.add(newTask)
+        res.status(201).json(createdTask)
+    } catch (err) {
+      res.status(500).json({ message: 'Error adding new task' })
+    }
+  })
+
+module.exports = router;
